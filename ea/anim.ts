c436ac356c89ba0d9e538b042ea964ab28aa17e7
Pepe Gotta <156920848+pepegotta34@users.noreply.github.com>
2024-02-10 17:50:38 +0200

    ea nostrud Lorem in labore

---
+++ ea/anim.ts
@@ -0,0 +1,2 @@
+Ad et qui labore quis consequat proident eu incididunt laboris commodo ipsum magna. Irure laborum mollit et velit veniam Lorem ipsum do laborum dolore minim. Id consectetur irure proident irure in excepteur anim quis. Fugiat do nostrud dolore sit do aliqua officia sit. Elit ut enim nisi exercitation labore veniam mollit.
+Ad amet dolor deserunt enim eiusmod aliquip Lorem aliquip consectetur. Aliqua eiusmod aliqua exercitation amet aliqua quis voluptate velit velit sint aliqua. Commodo ea nostrud aliqua culpa consectetur tempor enim aliqua eu proident irure elit.
